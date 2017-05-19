26c493005e7ad9b8b7a311fa13d322d1ffb745f8
Miika Pärni <miika.parni@helsinki.fi>
2017-05-19 12:21:25 +0300

    Express, Socket.io and basic HTML template added. Added a dataset module

--- index.js
+++ index.js
@@ -1,58 +1,27 @@
 var fs = require('fs');
 var dataArray = [];
+var express = require('express');
+var app = express();
 var urlPrefix = 'https://www.evira.fi';
-
-
-/*
-	returns an array of items according to search letter
-	parameter key is the letter to search
-*/
-var listByAlphabet = function(key) {
-	return dataArray.filter(function(item,index){
-       	if(item.SearchLetter === key) {
-       		return item;
-       	}
-    });
-};
-
-/*
-	returns a set of all letter keys for titles
-	helper to construct menu
-*/
-var listAllKeys = function() {
-	var values = new Set();
-	var newArr = dataArray.filter(function(item){
-    	values.add(item.SearchLetter);
-	});
-	return values;
-};
-
-/*
-	returns a set of all titles of ecodes
-*/
-var listTitles = function() {
-	var values = new Set();
-	var newArr = dataArray.filter(function(item){
-    	values.add(item.ENumberName);
-	});
-	return values;
-};
-
-/*
-	returns a set of all titles of ecodes
-*/
-var listCodes = function() {
-	var values = new Set();
-	var newArr = dataArray.filter(function(item){
-    	values.add(item.ENumber);
-	});
-	return values;
-};
+var dataSet = require("./dataset.js");
+var path    = require("path");
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 
 fs.readFile('ekoodit.json', 'utf8', function (err, data) {
     if (err) throw err;
     var obj = JSON.parse(data);
     dataArray = obj.eNumbers;
+
+    
+	app.listen(3000, function () {
+	  console.log('Example app listening on port 3000!')
+	});
+	app.get('/',function(req,res){
+  	res.sendFile(path.join(__dirname+'/index.html'));
+  		
+	});
+	
 
 });
