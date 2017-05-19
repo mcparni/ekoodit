348083dbd23d98f8bc923cce7d52f37a5b485106
Miika Pärni <miika.parni@helsinki.fi>
2017-05-19 14:22:03 +0300

    Basic functionality is there. Needs a little clean up and heroku hosting.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var fs = require('fs');
 var dataArray = [];
 var express = require('express');
 var app = express();
-var urlPrefix = 'https://www.evira.fi';
 var dataSet = require("./dataset.js");
 var path    = require("path");
 var http = require('http').Server(app);
@@ -14,14 +13,29 @@ fs.readFile('ekoodit.json', 'utf8', function (err, data) {
     var obj = JSON.parse(data);
     dataArray = obj.eNumbers;
 
-    
-	app.listen(3000, function () {
-	  console.log('Example app listening on port 3000!')
-	});
 	app.get('/',function(req,res){
-  	res.sendFile(path.join(__dirname+'/index.html'));
+  		res.sendFile(path.join(__dirname+'/index.html'));
+  		
+	});
+	app.get('/result',function(req,res){
+  		res.sendFile(path.join(__dirname+'/result.html'));
   		
 	});
 	
+	io.on('connection', function(socket){
+		socket.on('search', function(code){
+    		code = code.toUpperCase();
+    		var res = dataSet.listByCode(dataArray, code);
+    		
+    		if(res.length == 0)
+    			io.emit('results', 0);
+    		else
+    			io.emit('results', res[0]);
+  		});
+	});
+
+	http.listen(3000, function(){
+		console.log('kuunnellaan porttia *:3000');
+	});
 
 });
